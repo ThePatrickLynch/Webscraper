f2eb6e16f4f1b091f9e84e59894a106b9c8bd485
ThePatrickLynch <p.lynch@hull.ac.uk>
2015-04-27 21:15:13 +0100

    Sorted losing #'s and a bit more
    Much of the first stage is there. Need to cover whole site next and then
    onto analysis

--- getURL.R
+++ getURL.R
@@ -1,61 +1,84 @@
+##################################
+# Webscraper script
+#
+# This script will recurse through a website and create a cleaned text page for each webpage
+# encountered
+#
+##################################
+
+
 rm(list=ls())
 
 library("RCurl")
 library(XML)
 
-dir="d:/data/dir/"
+###################
+# just sort out my directory depending where I am working
+###################
+
+home <- T
+laptop = F
+dir="d:/data/dir" # default to work
+if (home) dir="e:/data/dir/" 
+if (laptop) dir <- "c:/data/dir"
+  
+####################
+# set starting point
+####################
+
 baseurl <- "http://www.birmingham.ac.uk"
-
 url <- paste(baseurl,"/index.aspx", sep="")
-file <- 1
-
 
 ######
 # function to read a webpage and write a file for each link on the page
 ######
 
 writeurltext <- function(url, filename) {
-  
+  url
   html <- getURL(url, followlocation = TRUE)
   html <- htmlParse(html, useInternal = TRUE)
   links <- as.vector(xpathSApply(html, "//a/@href")) # as.vector gets rid of the href attribute
-  
+
   # parse html
   txt <- xpathApply(html, "//body//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)]", xmlValue)
   
   #file <- paste(dir,as.character(file),'.txt',sep="")
-  file
   
-  sink(file)
+  sink(filename)
   cat(paste(txt), collapse = " ")
   sink()
   return(links)
 }
 
-# Get links from base page
+#################
+# Starting by getting links from first page into a cleaned list
+#################
+
 html <- getURL(url, followlocation = TRUE) # reads the main page
 html <- htmlParse(html, useInternal = TRUE) # parses the html
-links <- as.vector(xpathSApply(html, "//a/@href")) # gets the links, as.vector gets rid of the href attribute
+links <- as.vector(xpathSApply(html, "//a/@href")) # gets the links, as.vector gets rid of the href attribute cleaning
 
+# start cleaning
+links <- setdiff(links,'#') # setdiff is difference from links and '#' - so removes #'s
 
-idx <- substr(links,1,1) == '#'  # index those beginning with # - internal links
-links2 <- setdiff(links,idx) # setdiff is set union this gets those items not in idx
-
-idx
-links
-links2
-
-idx <- substr(links,1,1) == '/' # build an index list of those which are a subpage, I need the full url
+# make long urls for relative links
+idx <- substr(links,1,1) == '/' # build an index list of those which are a subpage ie. start with /
 links[idx] <- paste(baseurl,links[idx],sep="") # apply the concatenation to those items in the index
 
+# lose those that aren't birmingham
+idx <- substr(links,1,28) == 'http://www.birmingham.ac.uk/'
+links <- links[idx]
 
-#idx <- substr(links,1,27) == 'http://www.birmingham.ac.uk'
-#links2 <- intersect(links,idx)
+# send each line to the function which reads and outputs
+len=length(links)
+for (i in 1:len) {
+  filename <- paste(i,'.txt', sep="")
+  filename <- paste(dir,filename,sep='')
+  url <- paste(links[i], '/index.asp', sep="")
+  test <- writeurltext(url, filename)
+}
 
-links
+# now to work out how to navigate everything !!!
 
-links2
-
-test <- writeurltext(links[1])
-
+# then when I have all the files its time to analyse...
 
